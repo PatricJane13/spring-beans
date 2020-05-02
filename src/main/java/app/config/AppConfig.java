73191619f87fac0f60e4b9ce6c334e91f103dc24
Admin <13bombom666@mail.ru>
2020-05-02 14:00:09 +0300

    Initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,19 @@
+package app.config;
+
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+    @Bean(name="dog")
+    public Dog getDog(){
+        return new Dog();
+    }
+
+}
